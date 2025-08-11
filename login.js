b62d3f91e4518e1e08a12014c73af1d665b63cc1
Ponmani1021 <ponmani1021@gmail.com>
2025-08-11 12:51:27 +0530

    Completed

---
+++ login.js
@@ -0,0 +1,41 @@
+let form = document.getElementById("form")
+
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+
+    // getting the values from input box
+    let userName = document.getElementById("name");
+    let wNum = document.getElementById("num");
+
+    //definig default value
+    let crtUserName = "Ponmani"
+    let crtWNumber = "0123456789"
+
+    if((userName.value) === crtUserName && (wNum.value) === crtWNumber){
+        window.location.href = "profile.html"   //Navigate to another file
+    }
+    else{
+        alert("Invalid Username or Whatspp Number");
+        userName.value= "";
+        wNum.value = "";
+    }
+})
+
+// for selecting profile image
+
+let submitbutton = document.getElementById("submitbtn")
+let profileImage = document.getElementById("fileupload")
+let previewimg = document.getElementById("previewImage")
+
+profileImage.addEventListener('change', () => {
+    const file = profileImage.files[0];
+    if(file){
+        const reader = new FileReader();
+        reader.onload = function () {
+            previewimg.src = reader.result; 
+            localStorage.setItem('profileImage', reader.result);
+        }
+        reader.readAsDataURL(file);
+    }
+})
+
